3e3c42980409336580c5ac38ce6d138198c14b75
bala-amypo <bala@amypo.in>
2025-12-18 08:55:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Table(name="products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable=false)
+    private String name;
+    
+}
