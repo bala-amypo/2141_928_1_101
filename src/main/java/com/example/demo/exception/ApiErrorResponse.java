7c6755fa06c25eae9aa33138d0ba3412f2765d42
bala-amypo <bala@amypo.in>
2025-12-22 10:27:33 +0000

    User commit

--- src/main/java/com/example/demo/exception/ApiErrorResponse.java
+++ src/main/java/com/example/demo/exception/ApiErrorResponse.java
@@ -1,13 +1,18 @@
 package com.example.demo.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
 public class ApiErrorResponse {
 
     private int status;
     private String message;
     private long timestamp;
+
+    public ApiErrorResponse(int status, String message, long timestamp) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public int getStatus() { return status; }
+    public String getMessage() { return message; }
+    public long getTimestamp() { return timestamp; }
 }
