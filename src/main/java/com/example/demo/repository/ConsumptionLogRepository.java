c7afbf2fb6c4f9becb372b493c6c36810974497d
bala-amypo <bala@amypo.in>
2025-12-24 14:41:46 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/ConsumptionLogRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.ConsumptionLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ConsumptionLogRepository extends JpaRepository<ConsumptionLog, Long> {
+    List<ConsumptionLog> findByStockRecordId(Long stockRecordId);
+}
