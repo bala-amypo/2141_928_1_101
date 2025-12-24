086482ef44e30fa4606e5246a6f532701c9a0f2c
bala-amypo <bala@amypo.in>
2025-12-24 06:06:33 +0000

    User commit

--- src/main/java/com/example/demo/dto/UserRegisterDto.java
+++ src/main/java/com/example/demo/dto/UserRegisterDto.java
@@ -1,17 +1,22 @@
 package com.example.demo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.List;
 
-import java.util.Set;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserRegisterDto {
     private String name;
     private String email;
     private String password;
-    private Set<String> roles;
+    private List<String> roles;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public List<String> getRoles() { return roles; }
+    public void setRoles(List<String> roles) { this.roles = roles; }
 }
