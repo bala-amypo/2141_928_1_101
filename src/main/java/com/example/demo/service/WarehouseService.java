3a10c7a9e1706519bf38787074b64105a7f93c76
bala-amypo <bala@amypo.in>
2025-12-26 05:16:30 +0000

    User commit

--- src/main/java/com/example/demo/service/WarehouseService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.Warehouse;
-
-public interface WarehouseService {
-    Warehouse createWarehouse(Warehouse warehouse);
-    Warehouse getWarehouse(Long id);
-}
