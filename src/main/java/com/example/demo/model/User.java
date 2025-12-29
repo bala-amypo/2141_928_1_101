8c71d27fe63f373ae3ad5929be9f33ea38d890ca
bala-amypo <bala@amypo.in>
2025-12-29 08:02:12 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -13,19 +13,19 @@ import java.util.Set;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    private String name;
-    
-    @Column(unique = true)
-    private String email;
-    
-    private String password;
-    
-    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
-    @Enumerated(EnumType.STRING)
-    private Set<Role> roles;
-    
-    private LocalDateTime createdAt;
-}
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+            private Long id;
+                
+                    private String name;
+                        
+                            @Column(unique = true)
+                                private String email;
+                                    
+                                        private String password;
+                                            
+                                                @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+                                                    @Enumerated(EnumType.STRING)
+                                                        private Set<Role> roles;
+                                                            
+                                                                private LocalDateTime createdAt;
+                                                                }
